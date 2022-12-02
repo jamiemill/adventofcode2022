29211ab6208b1471539f75a11d1afb041d39423e
Jamie Mill <jamiermill@gmail.com>
2022-12-02 07:36:55 +0000

    Rename things

--- days/02.ts
+++ days/02.ts
@@ -1,31 +1,31 @@
-type Play = "ROCK" | "PAPER" | "SCISSORS";
+type Shape = "ROCK" | "PAPER" | "SCISSORS";
 type Result = "WIN" | "LOSS" | "DRAW";
 
-const theirPlays: { [key: string]: Play } = {
+const decodeThem: { [key: string]: Shape } = {
   A: "ROCK",
   B: "PAPER",
   C: "SCISSORS",
 };
 
-const myPlays: { [key: string]: Play } = {
+const decodeMe: { [key: string]: Shape } = {
   X: "ROCK",
   Y: "PAPER",
   Z: "SCISSORS",
 };
 
-const itemPoints = {
+const pointsForShape = {
   ROCK: 1,
   PAPER: 2,
   SCISSORS: 3,
 };
 
-const gamePoints = {
+const pointsForResult = {
   WIN: 6,
   DRAW: 3,
   LOSS: 0
 }
 
-function resultOf(them:Play, me:Play):Result {
+function resultOf(them:Shape, me:Shape):Result {
   if (them === me) {
     return "DRAW";
   } else if (them === "ROCK" && me === "PAPER") {
@@ -39,18 +39,18 @@ function resultOf(them:Play, me:Play):Result {
   }
 }
 
-type StrategyGuide = Array<{ them: Play; me: Play }>;
+type StrategyGuide = Array<{ them: Shape; me: Shape }>;
 
 function parseInput(input: string): StrategyGuide {
-  return input.split("\n").map((round) => {
-    const lineParts = round.split(" ");
-    return { them: theirPlays[lineParts[0]], me: myPlays[lineParts[1]] };
+  return input.split("\n").map((line) => {
+    const lineParts = line.split(" ");
+    return { them: decodeThem[lineParts[0]], me: decodeMe[lineParts[1]] };
   });
 }
 
 function scoreGame(strategy:StrategyGuide):number {
   return strategy.reduce((acc, item) => {
-    return acc + itemPoints[item.me] + gamePoints[resultOf(item.them, item.me)];
+    return acc + pointsForShape[item.me] + pointsForResult[resultOf(item.them, item.me)];
   },0)
 }
 
