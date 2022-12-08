56fc41edd6ac797d8ea129a70e995e01c7050ed9
Jamie Mill <jamiermill@gmail.com>
2022-12-08 07:35:25 +0000

    Refactor

--- days/08/index.ts
+++ days/08/index.ts
@@ -6,6 +6,13 @@ function parseInput(input: string): Grid {
   return input.split("\n").map((r) => r.split("").map(parseFloat));
 }
 
+function beforeAndAfter<T>(
+  list: Array<T>,
+  index: number,
+): [Array<T>, Array<T>] {
+  return [list.slice(0, index), list.slice(index + 1)];
+}
+
 function isVisible(grid: Grid, x: number, y: number): boolean {
   const thisTreeHeight = grid[y][x];
 
@@ -15,17 +22,15 @@ function isVisible(grid: Grid, x: number, y: number): boolean {
   if (y === 0 || y === (height - 1)) return true;
 
   const row = grid[y];
-  const rowBefore = row.slice(0, x);
-  const rowAfter = row.slice(x + 1);
-
+  const [rowBefore, rowAfter] = beforeAndAfter(row, x);
   const column = grid.map((r) => r[x]);
-  const columnBefore = column.slice(0, y);
-  const columnAfter = column.slice(y + 1);
+  const [columnBefore, columnAfter] = beforeAndAfter(column, y);
 
   const isShorter = (h: TreeHeight) => h < thisTreeHeight;
-  const sectionsToCheck = [rowBefore, rowAfter, columnBefore, columnAfter];
 
-  return !!sectionsToCheck.find((trees) => trees.every(isShorter));
+  return !![rowBefore, rowAfter, columnBefore, columnAfter].find((trees) =>
+    trees.every(isShorter)
+  );
 }
 
 export function part1(input: string): number {
