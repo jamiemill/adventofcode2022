a0f3dc422e6e1f63199898699cc711ae6e9ed702
Jamie Mill <jamiermill@gmail.com>
2022-12-08 12:33:01 +0000

    Solve day 8 part 2

--- days/08/index.ts
+++ days/08/index.ts
@@ -1,10 +1,8 @@
-import { flatten } from "https://cdn.skypack.dev/remeda?dts";
-
 type TreeHeight = number;
 type Row = TreeHeight[];
 type Grid = Row[];
 
-function parseInput(input: string): Grid {
+export function parseInput(input: string): Grid {
   return input.split("\n").map((r) => r.split("").map(parseFloat));
 }
 
@@ -50,21 +48,51 @@ export function part1(input: string): number {
   return visibleFromOutsideCount;
 }
 
-// function calculateViewScore(grid: Grid, x: number, y: number): number {
-// }
+function takeUntilFirstSameOrTaller(
+  trees: TreeHeight[],
+  tree: TreeHeight,
+): TreeHeight[] {
+  let i = trees.findIndex((t) => t >= tree);
+  if (i === -1) {
+    i = trees.length - 1;
+  }
+  return trees.slice(0, i + 1);
+}
 
-// export function part2(input: string): number {
-//   const grid = parseInput(input);
-//   let bestScore = 0;
-//   const width = grid[0].length;
-//   const height = grid.length;
-//   for (let y = 0; y < height; y++) {
-//     for (let x = 0; x < width; x++) {
-//       const score = calculateViewScore(grid, x, y);
-//       if (score > bestScore) {
-//         bestScore = score;
-//       }
-//     }
-//   }
-//   return bestScore;
-// }
+/**
+ * the view score is:
+ * the product of the count of trees seen in each direction
+ * where 'seen' means:
+ * - it's not blocked by any other tree (blocked means it's the same or taller than the starting tree)
+ */
+export function calculateViewScore(grid: Grid, x: number, y: number): number {
+  const thisTreeHeight = grid[y][x];
+
+  const row = grid[y];
+  const [rowBefore, rowAfter] = beforeAndAfter(row, x);
+  const column = grid.map((r) => r[x]);
+  const [columnBefore, columnAfter] = beforeAndAfter(column, y);
+
+  rowBefore.reverse();
+  columnBefore.reverse();
+
+  return [rowBefore, rowAfter, columnBefore, columnAfter].map((trees) =>
+    takeUntilFirstSameOrTaller(trees, thisTreeHeight).length
+  ).reduce((res, direction) => res * direction, 1);
+}
+
+export function part2(input: string): number {
+  const grid = parseInput(input);
+  let bestScore = 0;
+  const width = grid[0].length;
+  const height = grid.length;
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const score = calculateViewScore(grid, x, y);
+      if (score > bestScore) {
+        bestScore = score;
+      }
+    }
+  }
+  return bestScore;
+}
--- days/08/test.ts
+++ days/08/test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.165.0/testing/asserts.ts";
-import { part1 } from "./index.ts";
+import { calculateViewScore, parseInput, part1, part2 } from "./index.ts";
 
 const testDataFromExample = `30373
 25512
@@ -18,13 +18,18 @@ Deno.test("Day 8 Part 1", async (t) => {
   });
 });
 
-// Deno.test("Day 8 Part 2", async (t) => {
-//   await t.step("example", () => {
-//     assertEquals(part2(testDataFromExample), 0);
-//   });
+Deno.test("Day 8 Part 2", async (t) => {
+  await t.step("calcaulteViewScore", () => {
+    const grid = parseInput(testDataFromExample);
+    const result = calculateViewScore(grid, 2, 1);
+    assertEquals(result, 4);
+  });
+  await t.step("example", () => {
+    assertEquals(part2(testDataFromExample), 8);
+  });
 
-//   await t.step("answer", async () => {
-//     const input = await Deno.readTextFile(`./days/08/input.txt`);
-//     assertEquals(part2(input), 0);
-//   });
-// });
+  await t.step("answer", async () => {
+    const input = await Deno.readTextFile(`./days/08/input.txt`);
+    assertEquals(part2(input), 335580);
+  });
+});
